b2425944e492069b8b7d562bad8976c50b80fed9
JoaoMadeiraxyz <jaodisc@gmail.com>
2024-08-14 16:54:19 -0300

    feat: add api file

---
+++ internal/api/api.go
@@ -0,0 +1,53 @@
+package api
+
+import (
+	"net/http"
+	"sync"
+
+	"github.com/JoaoMadeiraxyz/ask-me-anything-server/internal/store/pgstore"
+	"github.com/JoaoMadeiraxyz/musify-server/internal/store/pgstore"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+)
+
+type apiHandler struct {
+	q  *pgstore.Queries
+	r  *chi.Mux
+	mu *sync.Mutex
+}
+
+func (h apiHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	h.r.ServeHTTP(w, r)
+}
+
+func NewHandler(q *pgstore.Queries) http.Handler {
+	a := apiHandler{
+		q:  q,
+		mu: &sync.Mutex{},
+	}
+
+	r := chi.NewRouter()
+	r.Use(middleware.RequestID, middleware.Recoverer, middleware.Logger)
+
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	r.Route("/api", func(r chi.Router) {
+		r.Route("/users", func(r chi.Router) {
+			r.Post("/", a.handleCreateUser)
+		})
+	})
+
+	a.r = r
+	return a
+}
+
+func (h apiHandler) handleCreateUser(w http.ResponseWriter, r *http.Request) {}
